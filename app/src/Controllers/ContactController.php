3c5911982939dac0546581168b405c17ec662a60
Moussadev1 <moussa.diakitevh@gmail.com>
2024-11-20 23:27:00 +0100

    Ajout de fetchone

--- app/src/Controllers/ContactController.php
+++ app/src/Controllers/ContactController.php
@@ -7,15 +7,21 @@ use App\Http\Response;
 
 class ContactController extends AbstractController
 {
-    public function process(Request $request): Response
+    public function process(Request $request, array $params = []): Response
     {
         if ($request->getMethod() === 'POST') {
             return $this->create($request);
         }
 
-        if($request->getMethod() === 'GET') {
+        if ($request->getMethod() === 'GET') {
             return $this->fetch();
         }
+
+        if ($request->getMethod() === 'GET' && isset($params['filename'])) {
+            return $this->fetchone($params['filename']);
+        }
+
+        return new Response('Methode now Allowed', 405);
     }
     public function create(Request $request): Response
     {
@@ -57,14 +63,14 @@ class ContactController extends AbstractController
             );
         }
 
-        $responseContent = json_encode([
-            'file' => date('Y-m-d_H-i-s', $timestamp) . '_' . $body['email']
-        ]);
-
-        return new Response(json_encode(['file' => date('Y-m-d_H-i-s', $timestamp) . '_' . $body['email']]), 201, ['Content-Type' => 'application/json']);
+        return new Response(
+            json_encode(['file' => date('Y-m-d_H-i-s', $timestamp) . '_' . $body['email']]),
+            200,
+            ['Content-Type' => 'application/json']
+        );
     }
 
-    private function fetch(): Response
+    public function fetch(): Response
     {
         $directory = __DIR__ . '/../../var/contact/';
         $files = glob($directory . '*.json');
@@ -83,6 +89,23 @@ class ContactController extends AbstractController
             ['Content-Type' => 'application/json']
         );
     }
+
+    public function fetchone(string $filename): Response
+    {
+        $filePath = __DIR__ . '/../../var/contact/' . $filename . '.json';
+
+        if (!file_exists($filePath)) {
+            return new Response(
+                json_encode(["error" => "Contact not found"]),
+                404,
+                ['Content-Type' => 'application/json']
+            );
+        }
+
+        $content = file_get_contents($filePath);
+        return new Response($content, 200, ['Content-Type' => 'application/json']);
+    }
+
 
     private function response(array $body, int $status): void
     {
